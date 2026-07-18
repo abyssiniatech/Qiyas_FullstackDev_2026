2de01db1de0a84d5aedad3e6c7e71467011fcf13
meyatech <surafelmegist2015@gmail.com>
2026-07-18 02:01:38 -0700

    add Hello :

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec8ca8971fd18b80f2602a959938080b70789131")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064e1b22a53868e98b4dbd44426e55d87e6824ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
