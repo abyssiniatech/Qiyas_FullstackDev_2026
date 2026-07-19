74bdfedeb9bee9f7b1d6d111266baeafc566ae60
meyatech <surafelmegist2015@gmail.com>
2026-07-19 01:44:14 -0700

    add the final module 5 commit ...

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff71d159e2cbc9e6129bfbb4314cba920255976f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e598a31f31cf5a949fc9b1f70c8b67a7b8f99033")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
