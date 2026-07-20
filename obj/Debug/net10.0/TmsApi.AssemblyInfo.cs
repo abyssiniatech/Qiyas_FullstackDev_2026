78e0cd1885e69ea07ed6a9fa4b75d4d7f6bb5894
meyatech <surafelmegist2015@gmail.com>
2026-07-20 02:12:43 -0700

    add initial commit

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc508531d58f4b2c5ae843d6b0274bb4ddc9b4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ab2646341c44d73e20dc3d11593adf896386c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
